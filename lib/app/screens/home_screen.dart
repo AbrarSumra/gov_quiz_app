47f671dd8666c268db881545305faea070857e6f
Abrar Khira <133484195+AbrarSumra@users.noreply.github.com>
2024-09-15 15:21:40 +0530

    Initial Commit 2

--- lib/app/screens/home_screen.dart
+++ lib/app/screens/home_screen.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gov_quiz_app/app/constants/app_colors/app_colors.dart';
-import 'package:gov_quiz_app/app/constants/widgets/button_l.dart';
-import 'package:gov_quiz_app/app/constants/widgets/button_xs.dart';
-import 'package:gov_quiz_app/app/constants/widgets/custom_presseable_button.dart';
 import 'package:gov_quiz_app/app/screens/app_bar.dart';
 import 'package:gov_quiz_app/app/screens/drawer_screen.dart';
-
-import '../constants/widgets/button_s.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
